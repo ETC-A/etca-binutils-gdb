1ec5941bc77dfb1ace5c00b9728f25ee6d952e12
MegaIng <trampchamp@hotmail.de>
2025-11-01 01:42:58 +0100

    Remove debug prints + Fixup EXTEND_MASK macro

--- bfd/elf32-etca.c
+++ bfd/elf32-etca.c
@@ -775,7 +775,7 @@ uint8_t number_of_needed_bits(int64_t value) {
     return 64 - __builtin_clrsbll(value);
 }
 
-#define EXTEND_MASK(bit)   	((bit) == 64 ? -1LL : ((1ULL << (bit)) - 1ULL))
+#define EXTEND_MASK(bit)   	((bit) == 64 ? -1ULL : ((1ULL << (bit)) - 1ULL))
 #define SIGN_EXTEND(value, bit) (((value & EXTEND_MASK(bit)) ^ (1ULL << (bit - 1))) - (1ULL << (bit - 1)))
 
 static uint8_t size_to_width[4] = {8, 16, 32, 64};
@@ -792,11 +792,6 @@ etca_calc_mov_ri(const struct etca_cpuid *current_cpuid ATTRIBUTE_UNUSED, int8_t
     }
     if (size < 0 || size > 3) {
 	abort();
-    }
-
-    if (value_pointer) {
-	printf("immediate value: %ld\n", *value_pointer);
-    	printf("bits: %d\n", bits);
     }
 
     enum elf_etca_reloc_type ret = R_ETCA_NONE;
